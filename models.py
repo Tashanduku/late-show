add6d49a04d736e4595ef0b532735dbb9d2aa5a3
Natasha Nduku <natashanduku0@gmail.com>
2025-04-07 07:08:18 +0300

    adds tables for appearance

--- models.py
+++ models.py
@@ -36,3 +36,12 @@ class Guest(db.Model):
             "name": self.name,
             "occupation": self.occupation
         }
+
+
+    class Appearance(db.Model):
+    __tablename__ = 'appearances'
+
+    id = db.Column(db.Integer, primary_key=True)
+    rating = db.Column(db.Integer, nullable=False)
+    episode_id = db.Column(db.Integer, db.ForeignKey('episodes.id'), nullable=False)
+    guest_id = db.Column(db.Integer, db.ForeignKey('guests.id'), nullable=False)
