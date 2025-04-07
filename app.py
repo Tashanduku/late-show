6beb58f68e8fc57e62a9529746115e933761f4ee
Natasha Nduku <natashanduku0@gmail.com>
2025-04-07 07:41:01 +0300

    adds root to app

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask
 from flask_migrate import Migrate
 from flask_restful import Api
 from models import db
-from routes import EpisodesResource, EpisodeById, GuestsResource, AppearancesResource
+from routes import EpisodesResource, EpisodeById, GuestsResource, AppearancesResource, RootResource
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///project.db'
@@ -12,24 +12,13 @@ db.init_app(app)
 migrate = Migrate(app, db)
 api = Api(app)
 
-# Add API resources
+
+api.add_resource(RootResource, '/')
+
 api.add_resource(EpisodesResource, '/episodes')
 api.add_resource(EpisodeById, '/episodes/<int:id>')
 api.add_resource(GuestsResource, '/guests')
 api.add_resource(AppearancesResource, '/appearances')
-
-# Add a route for the root URL
-@app.route('/')
-def index():
-    return jsonify({
-        "message": "Welcome to the Episodes API",
-        "endpoints": {
-            "episodes": "/episodes",
-            "episode_by_id": "/episodes/<id>",
-            "guests": "/guests",
-            "appearances": "/appearances"
-        }
-    })
 
 if __name__ == '__main__':
     app.run(debug=True, port=8080)
