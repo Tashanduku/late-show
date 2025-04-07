3c975a8e18faeda3189275bc3140513fad0c5eaf
Natasha Nduku <natashanduku0@gmail.com>
2025-04-07 07:01:46 +0300

    initialize sql alchemy for episode

--- models.py
+++ models.py
@@ -2,3 +2,12 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import validates
 
 db = SQLAlchemy()
+
+class Episode(db.Model):
+    __tablename__ = 'episodes'
+
+    id = db.Column(db.Integer, primary_key=True)
+    date = db.Column(db.String, nullable=False)
+    number = db.Column(db.Integer, nullable=False)
+
+   
