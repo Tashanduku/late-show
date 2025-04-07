49008d8edc263d98c76bcfa57a98ccd2fa6a4f70
Natasha Nduku <natashanduku0@gmail.com>
2025-04-07 07:14:30 +0300

    add episresource with get points

--- routes.py
+++ routes.py
@@ -2,3 +2,8 @@ from flask import request, jsonify
 from flask_restful import Resource
 from models import db, Episode, Guest, Appearance
 
+class EpisodesResource(Resource):
+    def get(self):
+        episodes = Episode.query.all()
+        return [ {"id": e.id, "date": e.date, "number": e.number} for e in episodes ], 200
+
