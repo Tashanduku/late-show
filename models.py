ca456ebf2912df0f09472a02fe2ce5220e1243f0
Natasha Nduku <natashanduku0@gmail.com>
2025-04-07 07:09:57 +0300

    adds sterilization

--- models.py
+++ models.py
@@ -38,10 +38,26 @@ class Guest(db.Model):
         }
 
 
-    class Appearance(db.Model):
+class Appearance(db.Model):
     __tablename__ = 'appearances'
 
     id = db.Column(db.Integer, primary_key=True)
     rating = db.Column(db.Integer, nullable=False)
     episode_id = db.Column(db.Integer, db.ForeignKey('episodes.id'), nullable=False)
     guest_id = db.Column(db.Integer, db.ForeignKey('guests.id'), nullable=False)
+
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "rating": self.rating,
+            "guest_id": self.guest_id,
+            "episode_id": self.episode_id,
+            "guest": self.guest.to_dict(),
+            "episode": {
+                "id": self.episode.id,
+                "date": self.episode.date,
+                "number": self.episode.number
+            }
+        }
+
