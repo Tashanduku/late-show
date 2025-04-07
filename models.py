83a2d5033272fabd5f377539185c1c9f7c4a000c
Natasha Nduku <natashanduku0@gmail.com>
2025-04-07 07:10:38 +0300

    adds validation methord

--- models.py
+++ models.py
@@ -46,6 +46,12 @@ class Appearance(db.Model):
     episode_id = db.Column(db.Integer, db.ForeignKey('episodes.id'), nullable=False)
     guest_id = db.Column(db.Integer, db.ForeignKey('guests.id'), nullable=False)
 
+    @validates('rating')
+    def validate_rating(self, key, rating):
+        if not (1 <= rating <= 5):
+            raise ValueError("Rating must be between 1 and 5")
+        return rating
+
 
     def to_dict(self):
         return {
