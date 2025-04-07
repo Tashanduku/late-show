99f4cede14d17e2187e838d0643ce120593e27c5
Natasha Nduku <natashanduku0@gmail.com>
2025-04-07 07:02:51 +0300

    Add relationship to Appearance model

--- models.py
+++ models.py
@@ -10,4 +10,6 @@ class Episode(db.Model):
     date = db.Column(db.String, nullable=False)
     number = db.Column(db.Integer, nullable=False)
 
-   
+    appearances = db.relationship('Appearance', backref='episode', cascade="all, delete")
+
+    
