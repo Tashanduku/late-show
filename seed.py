dc4a001b261a815d10bce0f9f03810227b446604
Natasha Nduku <natashanduku0@gmail.com>
2025-04-14 23:45:59 +0300

    Add guest records with name and occupation fields

--- seed.py
+++ seed.py
@@ -2,7 +2,15 @@ from app import app
 from models import db, Episode, Guest, Appearance
 
 print("Seeding episodes...")
-        episode1 = Episode(date="1/11/99", number=1)
-        episode2 = Episode(date="1/12/99", number=2)
-        episode3 = Episode(date="1/13/99", number=3) 
+        episode1 = Episode(date="1/11/23", number=1)
+        episode2 = Episode(date="1/12/24", number=2)
+        episode3 = Episode(date="1/13/25", number=3) 
         db.session.add_all([episode1, episode2, episode3])
+
+
+
+print("Seeding guests...")
+        guest1 = Guest(name="Chunxy", occupation="Podcuster")
+        guest2 = Guest(name="Dennis", occupation="comedian")
+        guest3 = Guest(name="Willy Paul", occupation="musician")
+        db.session.add_all([guest1, guest2, guest3])
