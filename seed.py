c457066d35121b49410abb4003f796659b10e514
Natasha Nduku <natashanduku0@gmail.com>
2025-04-14 23:43:51 +0300

    Create episode seed data with required fields

--- seed.py
+++ seed.py
@@ -1,2 +1,8 @@
 from app import app
 from models import db, Episode, Guest, Appearance
+
+print("Seeding episodes...")
+        episode1 = Episode(date="1/11/99", number=1)
+        episode2 = Episode(date="1/12/99", number=2)
+        episode3 = Episode(date="1/13/99", number=3) 
+        db.session.add_all([episode1, episode2, episode3])
