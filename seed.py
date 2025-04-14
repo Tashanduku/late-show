4714f111bfef255ac58b7240a7124955ea652654
Natasha Nduku <natashanduku0@gmail.com>
2025-04-14 23:51:43 +0300

    changes the appearance and finalize

--- seed.py
+++ seed.py
@@ -1,25 +1,36 @@
 from app import app
 from models import db, Episode, Guest, Appearance
 
-print("Seeding episodes...")
-        episode1 = Episode(date="1/11/23", number=1)
-        episode2 = Episode(date="1/12/24", number=2)
-        episode3 = Episode(date="1/13/25", number=3) 
-        db.session.add_all([episode1, episode2, episode3])
-
-
-
-print("Seeding guests...")
-        guest1 = Guest(name="Chunxy", occupation="Podcuster")
-        guest2 = Guest(name="Dennis", occupation="comedian")
-        guest3 = Guest(name="Willy Paul", occupation="musician")
-        db.session.add_all([guest1, guest2, guest3])
-
-
-print("Seeding appearances...")
-        appearance1 = Appearance(episode_id=1, guest_id=1, rating=4)
-        appearance2 = Appearance(episode_id=1, guest_id=2, rating=4)
-        appearance3 = Appearance(episode_id=2, guest_id=3, rating=5)
-        appearance4 = Appearance(episode_id=3, guest_id=1, rating=1)
-        appearance5 = Appearance(episode_id=3, guest_id=2, rating=2)
-        db.session.add_all([appearance1, appearance2, appearance3, appearance4, appearance5])
+with app.app_context():
+    print("Seeding database...")
+    
+    db.session.query(Appearance).delete()
+    db.session.query(Guest).delete()
+    db.session.query(Episode).delete()
+    
+    episodes = [
+        Episode(date="1/11/99", number=1),
+        Episode(date="1/12/99", number=2),
+        Episode(date="1/13/99", number=3),
+    ]
+    
+    guests = [
+        Guest(name="Chunxy", occupation="artist"),
+        Guest(name="Dennis", occupation="comedian"),
+        Guest(name="Willy Paul", occupation="musician"),
+    ]
+    
+    db.session.add_all(episodes + guests)
+    db.session.commit()
+    
+    appearances = [
+        Appearance(episode=episodes[0], guest=guests[0], rating=4),
+        Appearance(episode=episodes[0], guest=guests[1], rating=5),
+        Appearance(episode=episodes[1], guest=guests[2], rating=5),
+        Appearance(episode=episodes[2], guest=guests[0], rating=3),
+        Appearance(episode=episodes[2], guest=guests[1], rating=2),
+    ]
+    
+    db.session.add_all(appearances)
+    db.session.commit()
+    print("Seeding complete!")
